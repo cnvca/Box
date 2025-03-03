61270f5262ace0f5241dbaf2aa91a79551aaf43f
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-03 13:28:29 +0800

    Update OkDns.java

--- app/src/main/java/com/github/catvod/net/OkDns.java
+++ app/src/main/java/com/github/catvod/net/OkDns.java
@@ -1,1 +1,27 @@
+package com.github.catvod.net;
 
+import androidx.annotation.NonNull;
+
+import com.github.catvod.utils.Util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import okhttp3.Dns;
+import okhttp3.dnsoverhttps.DnsOverHttps;
+
+public class OkDns implements Dns {
+
+    private final ConcurrentHashMap<String, String> map;
+    private DnsOverHttps doh;
+
+    public OkDns() {
+        this.map = new ConcurrentHashMap<>();
+    }
+
+    public void setDoh(DnsOverHttps doh) {
+        this.doh = doh;
+    }
