cb7ccfafa33fb5adfc2fdb62532f49675b03b197
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-03 19:40:09 +0800

    Update IjkmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/IjkmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/IjkmPlayer.java
@@ -11,6 +11,7 @@ import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.MD5;
 import com.github.tvbox.osc.util.PlayerHelper;
 import com.orhanobut.hawk.Hawk;
+import com.github.catvod.net.OkHttp;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
@@ -20,6 +21,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import okhttp3.OkHttpClient;
 import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 import tv.danmaku.ijk.media.player.misc.ITrackInfo;
@@ -54,11 +56,14 @@ public class IjkmPlayer extends IjkPlayer {
                 }
             }
         }
-        //开启内置字幕
+        // 开启内置字幕
         mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "subtitle", 1);
+        // 清除 DNS 缓存
         mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "dns_cache_clear", 1);
+        // 禁用 DNS 缓存
         mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "dns_cache_timeout", -1);
-        mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT,"safe",0);
+        // 设置安全模式
+        mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "safe", 0);
         super.setOptions();
     }
 
@@ -66,11 +71,13 @@ public class IjkmPlayer extends IjkPlayer {
     public void setDataSource(String path, Map<String, String> headers) {
         try {
             if (path != null && !TextUtils.isEmpty(path)) {
-                if(path.startsWith("rtsp")){
+                if (path.startsWith("rtsp")) {
+                    // RTSP 协议配置
                     mMediaPlayer.setOption(1, "infbuf", 1);
                     mMediaPlayer.setOption(1, "rtsp_transport", "tcp");
                     mMediaPlayer.setOption(1, "rtsp_flags", "prefer_tcp");
                 } else if (!path.contains(".m3u8") && (path.contains(".mp4") || path.contains(".mkv") || path.contains(".avi"))) {
+                    // 本地文件缓存配置
                     if (Hawk.get(HawkConfig.IJK_CACHE_PLAY, false)) {
                         String cachePath = FileUtils.getExternalCachePath() + "/ijkcaches/";
                         String cacheMapPath = cachePath;
@@ -92,15 +99,19 @@ public class IjkmPlayer extends IjkPlayer {
         } catch (Exception e) {
             mPlayerEventListener.onError(-1, PlayerHelper.getRootCauseMessage(e));
         }
-        //mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "protocol_whitelist", "ijkio,ffio,async,cache,crypto,file,http,https,ijkhttphook,ijkinject,ijklivehook,ijklongurl,ijksegment,ijktcphook,pipe,rtp,tcp,tls,udp,ijkurlhook,data");
+
+        // 设置协议白名单
         mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "protocol_whitelist", "ijkio,ffio,async,cache,crypto,file,http,https,ijkhttphook,ijkinject,ijklivehook,ijklongurl,ijksegment,ijktcphook,pipe,rtp,tcp,tls,udp,ijkurlhook,data,concat,subfile,ffconcat");
-        //mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0L);
-//        try {
-//            path = encodeSpaceChinese(path);//会导致本地文件无法播放，故注释掉
-//        } catch (Exception ignored) {
-//
-//        }
-        super.setDataSource(path, headers);
+
+        // 使用 OkHttp 客户端进行 HTTP/HTTPS 请求
+        if (path.startsWith("http")) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .dns(OkHttp.dns()) // 使用自定义 DNS 解析器
+                    .build();
+            mMediaPlayer.setDataSource(new OkHttpDataSourceProvider(client, headers, path).getUri());
+        } else {
+            super.setDataSource(path, headers);
+        }
     }
 
     private String encodeSpaceChinese(String str) throws UnsupportedEncodingException {
@@ -120,12 +131,13 @@ public class IjkmPlayer extends IjkPlayer {
             mPlayerEventListener.onError(-1, PlayerHelper.getRootCauseMessage(e));
         }
     }
+
     private void setDataSourceHeader(Map<String, String> headers) {
         if (headers != null && !headers.isEmpty()) {
             String userAgent = headers.get("User-Agent");
             if (!TextUtils.isEmpty(userAgent)) {
                 mMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "user_agent", userAgent);
-                // 移除header中的User-Agent，防止重复
+                // 移除 header 中的 User-Agent，防止重复
                 headers.remove("User-Agent");
             }
             if (headers.size() > 0) {
@@ -142,6 +154,7 @@ public class IjkmPlayer extends IjkPlayer {
             }
         }
     }
+
     public TrackInfo getTrackInfo() {
         IjkTrackInfo[] trackInfo = mMediaPlayer.getTrackInfo();
         if (trackInfo == null) return null;
@@ -150,7 +163,7 @@ public class IjkmPlayer extends IjkPlayer {
         int audioSelected = mMediaPlayer.getSelectedTrack(ITrackInfo.MEDIA_TRACK_TYPE_AUDIO);
         int index = 0;
         for (IjkTrackInfo info : trackInfo) {
-            if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_AUDIO) {//音轨信息
+            if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_AUDIO) { // 音轨信息
                 String trackName = (data.getAudio().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean t = new TrackInfoBean();
                 t.name = trackName;
@@ -159,7 +172,7 @@ public class IjkmPlayer extends IjkPlayer {
                 t.selected = index == audioSelected;
                 data.addAudio(t);
             }
-            if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT) {//内置字幕
+            if (info.getTrackType() == ITrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT) { // 内置字幕
                 String trackName = (data.getSubtitle().size() + 1) + "：" + info.getInfoInline();
                 TrackInfoBean t = new TrackInfoBean();
                 t.name = trackName;
@@ -180,5 +193,4 @@ public class IjkmPlayer extends IjkPlayer {
     public void setOnTimedTextListener(IMediaPlayer.OnTimedTextListener listener) {
         mMediaPlayer.setOnTimedTextListener(listener);
     }
-
 }
