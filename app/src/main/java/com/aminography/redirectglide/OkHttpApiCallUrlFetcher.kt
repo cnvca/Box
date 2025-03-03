d6ab7146a37289c0c224ca7f9cca83476f8f8999
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-03 20:33:54 +0800

    Update OkHttpApiCallUrlFetcher.kt

--- app/src/main/java/com/aminography/redirectglide/OkHttpApiCallUrlFetcher.kt
+++ app/src/main/java/com/aminography/redirectglide/OkHttpApiCallUrlFetcher.kt
@@ -1,33 +1,55 @@
 package com.aminography.redirectglide
 
-import com.bumptech.glide.load.HttpException
-import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.data.DataFetcher
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody
 import java.io.IOException
 
-/**
- * Fetches an [GlideUrl] using the okhttp library.
- */
 class OkHttpApiCallUrlFetcher(
-    client: Call.Factory,
-    url: BaseApiCallGlideUrl
-) : OkHttpRedirectUrlFetcher(client, url), Callback {
+    private val client: OkHttpClient,
+    private val request: Request
+) : DataFetcher<ResponseBody> {
 
-    override fun consumeResponse(response: Response) {
-        responseBody = response.body()
-        if (response.isSuccessful && responseBody != null) {
-            try {
-                val sourceUrl = (url as BaseApiCallGlideUrl).extractImageUrl(responseBody?.string())
-                callback?.onDataReady(GlideUrl(sourceUrl))
-            } catch (e: IOException) {
-                callback?.onLoadFailed(HttpException(response.message(), response.code()))
-                e.printStackTrace()
+    private var call: Call? = null
+    private var responseBody: ResponseBody? = null
+
+    override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in ResponseBody>) {
+        call = client.newCall(request)
+        call?.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onLoadFailed(e)
             }
-        } else {
-            callback?.onLoadFailed(HttpException(response.message(), response.code()))
-        }
+
+            override fun onResponse(call: Call, response: Response) {
+                responseBody = response.body
+                if (response.isSuccessful) {
+                    callback.onDataReady(responseBody)
+                } else {
+                    callback.onLoadFailed(IOException("Request failed with code: ${response.code}, message: ${response.message}"))
+                }
+            }
+        })
     }
 
+    override fun cleanup() {
+        responseBody?.close()
+    }
+
+    override fun cancel() {
+        call?.cancel()
+    }
+
+    override fun getDataClass(): Class<ResponseBody> {
+        return ResponseBody::class.java
+    }
+
+    override fun getDataSource(): DataSource {
+        return DataSource.REMOTE
+    }
 }
