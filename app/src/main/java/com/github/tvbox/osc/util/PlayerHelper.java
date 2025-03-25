af759df85e2b0e41ab9c2baf9f5be4fcf08a00e2
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-25 22:37:26 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -1,6 +1,7 @@
 package com.github.tvbox.osc.util;
 
 import android.content.Context;
+
 import com.github.tvbox.osc.api.ApiConfig;
 import com.github.tvbox.osc.bean.IJKCode;
 import com.github.tvbox.osc.player.EXOmPlayer;
@@ -8,8 +9,10 @@ import com.github.tvbox.osc.player.IjkmPlayer;
 import com.github.tvbox.osc.player.render.SurfaceRenderViewFactory;
 import com.github.tvbox.osc.ui.activity.ItvDns;
 import com.orhanobut.hawk.Hawk;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 import xyz.doikki.videoplayer.aliplayer.AliyunMediaPlayerFactory;
 import xyz.doikki.videoplayer.player.AndroidMediaPlayerFactory;
@@ -20,13 +23,12 @@ import xyz.doikki.videoplayer.render.RenderViewFactory;
 import xyz.doikki.videoplayer.render.TextureRenderViewFactory;
 
 public class PlayerHelper {
-    // 移除所有不正确的@Override注解
-    // 保持与xyz.doikki.videoplayer:dkplayer-ui:3.3.7兼容的API调用方式
-    
+    private static final String TAG = "PlayerHelper";
+
     public static String rewriteProxyUrl(String url) {
         try {
-            if (url != null && url.startsWith("http://127.0.0.1:9978")) {
-                return url.replace("http://127.0.0.1:9978/?channel-id=", "");
+            if (url != null && url.startsWith("http://127.0.0.1:9978/?channel-id=")) {
+                return url.split("channel-id=")[1];
             }
             return url;
         } catch (Exception e) {
@@ -34,99 +36,74 @@ public class PlayerHelper {
         }
     }
 
+    public static void updateCfg(VideoView videoView) {
+        updateCfg(videoView, null, -1);
+    }
+
     public static void updateCfg(VideoView videoView, JSONObject playerCfg) {
-        // 保持原有逻辑但移除错误覆盖
-        PlayerFactory playerFactory;
-        if (Hawk.get(HawkConfig.PLAY_TYPE, 0) == 1) {
-            playerFactory = new PlayerFactory<IjkmPlayer>() {
-                @Override
-                public IjkmPlayer createPlayer(Context context) {
-                    return new IjkmPlayer(context, null) {
-                        // 正确的重写方式（与dkplayer 3.3.7兼容）
-                        @Override
-                        public void setDataSource(String path) {
-                            try {
-                                super.setDataSource(rewriteProxyUrl(path));
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-                }
-            };
-        }
+        updateCfg(videoView, playerCfg, -1);
+    }
 
     public static void updateCfg(VideoView videoView, JSONObject playerCfg, int forcePlayerType) {
         int playerType = Hawk.get(HawkConfig.PLAY_TYPE, 0);
         int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
         String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "软解码");
         int scale = Hawk.get(HawkConfig.PLAY_SCALE, 0);
-        
-        try {
-            playerType = playerCfg.getInt("pl");
-            ijkCode = playerCfg.getString("ijk");
-            scale = playerCfg.getInt("sc");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        
-        if (forcePlayerType >= 0) playerType = forcePlayerType;
-        IJKCode codec = ApiConfig.get().getIJKCodec(ijkCode);
-        
-        PlayerFactory playerFactory;
-        if (playerType == 1) {
-            playerFactory = new PlayerFactory<IjkmPlayer>() {
-                @Override
-                public IjkmPlayer createPlayer(Context context) {
-                    return new IjkmPlayer(context, codec) {
-                        @Override
-                        public void setDataSource(String path) throws IOException {
-                            super.setDataSource(rewriteProxyUrl(path));
-                        }
-                    };
-                }
-            };
-        } else if (playerType == 2) {
-            playerFactory = new PlayerFactory<EXOmPlayer>() {
-                @Override
-                public EXOmPlayer createPlayer(Context context) {
-                    return new EXOmPlayer(context) {
-                        @Override
-                        public void setDataSource(String path) {
-                            super.setDataSource(rewriteProxyUrl(path));
-                        }
-                    };
-                }
-            };
-        } else if (playerType == 3) {
-            playerFactory = AliyunMediaPlayerFactory.create();
-        } else {
-            playerFactory = AndroidMediaPlayerFactory.create();
-        }
 
-        RenderViewFactory renderViewFactory = null;
-        if (playerType == 2) {
-            renderViewFactory = PlayerViewRenderViewFactory.create(renderType);
-        } else {
-            switch (renderType) {
-                case 0:
-                default:
-                    renderViewFactory = TextureRenderViewFactory.create();
-                    break;
-                case 1:
-                    renderViewFactory = SurfaceRenderViewFactory.create();
-                    break;
+        if (playerCfg != null) {
+            try {
+                playerType = playerCfg.getInt("pl");
+                ijkCode = playerCfg.getString("ijk");
+                scale = playerCfg.getInt("sc");
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
+
+        if (forcePlayerType >= 0) {
+            playerType = forcePlayerType;
+        }
+
+        PlayerFactory playerFactory = createPlayerFactory(playerType, ijkCode);
+        RenderViewFactory renderViewFactory = createRenderViewFactory(playerType, renderType);
 
         videoView.setPlayerFactory(playerFactory);
         videoView.setRenderViewFactory(renderViewFactory);
         videoView.setScreenScaleType(scale);
     }
 
+    private static PlayerFactory createPlayerFactory(int playerType, String ijkCode) {
+        switch (playerType) {
+            case 1:
+                return new PlayerFactory<IjkmPlayer>() {
+                    @Override
+                    public IjkmPlayer createPlayer(Context context) {
+                        return new IjkmPlayer(context, ApiConfig.get().getIJKCodec(ijkCode));
+                    }
+                };
+            case 2:
+                return new PlayerFactory<EXOmPlayer>() {
+                    @Override
+                    public EXOmPlayer createPlayer(Context context) {
+                        return new EXOmPlayer(context);
+                    }
+                };
+            case 3:
+                return AliyunMediaPlayerFactory.create();
+            default:
+                return AndroidMediaPlayerFactory.create();
+        }
+    }
+
+    private static RenderViewFactory createRenderViewFactory(int playerType, int renderType) {
+        if (playerType == 2) {
+            return PlayerViewRenderViewFactory.create(renderType);
+        }
+        return renderType == 1 ? SurfaceRenderViewFactory.create() : TextureRenderViewFactory.create();
+    }
+
     public static void init() {
         IjkMediaPlayer.loadLibrariesOnce(null);
-        // 初始化IJK播放器代理设置
         System.setProperty("http.proxyHost", "127.0.0.1");
         System.setProperty("http.proxyPort", String.valueOf(ItvDns.PORT));
         System.setProperty("https.proxyHost", "127.0.0.1");
@@ -134,60 +111,36 @@ public class PlayerHelper {
     }
 
     public static String getPlayerName(int playType) {
-        if (playType == 1) {
-            return "IJK";
-        } else if (playType == 2) {
-            return "Exo";
-        } else if (playType == 3) {
-            return "阿里";
-        } else if (playType == 10) {
-            return "MX";
-        } else if (playType == 11) {
-            return "Reex";
-        } else if (playType == 12) {
-            return "Kodi";
-        } else {
-            return "系统";
+        switch (playType) {
+            case 1: return "IJK";
+            case 2: return "Exo";
+            case 3: return "阿里";
+            case 10: return "MX";
+            case 11: return "Reex";
+            case 12: return "Kodi";
+            default: return "系统";
         }
     }
 
     public static String getRenderName(int renderType) {
-        if (renderType == 1) {
-            return "SurfaceView";
-        } else {
-            return "TextureView";
-        }
+        return renderType == 1 ? "SurfaceView" : "TextureView";
     }
 
     public static String getScaleName(int screenScaleType) {
-        String scaleText = "默认";
         switch (screenScaleType) {
-            case VideoView.SCREEN_SCALE_DEFAULT:
-                scaleText = "默认";
-                break;
-            case VideoView.SCREEN_SCALE_16_9:
-                scaleText = "16:9";
-                break;
-            case VideoView.SCREEN_SCALE_4_3:
-                scaleText = "4:3";
-                break;
-            case VideoView.SCREEN_SCALE_MATCH_PARENT:
-                scaleText = "填充";
-                break;
-            case VideoView.SCREEN_SCALE_ORIGINAL:
-                scaleText = "原始";
-                break;
-            case VideoView.SCREEN_SCALE_CENTER_CROP:
-                scaleText = "裁剪";
-                break;
+            case VideoView.SCREEN_SCALE_DEFAULT: return "默认";
+            case VideoView.SCREEN_SCALE_16_9: return "16:9";
+            case VideoView.SCREEN_SCALE_4_3: return "4:3";
+            case VideoView.SCREEN_SCALE_MATCH_PARENT: return "填充";
+            case VideoView.SCREEN_SCALE_ORIGINAL: return "原始";
+            case VideoView.SCREEN_SCALE_CENTER_CROP: return "裁剪";
+            default: return "默认";
         }
-        return scaleText;
     }
 
     public static String getRootCauseMessage(Throwable th) {
-        for (int i=0; i<10; i++) {
-            if (th.getCause() == null) return th.getLocalizedMessage();
-            else th = th.getCause();
+        for (int i = 0; i < 10 && th.getCause() != null; i++) {
+            th = th.getCause();
         }
         return th.getLocalizedMessage();
     }
