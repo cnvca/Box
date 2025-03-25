680a26fdd474333ac049ad5b3c393103a889d9a3
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-25 17:44:09 +0800

    Update ProxyUrlHelper.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/ProxyUrlHelper.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/ProxyUrlHelper.java
@@ -1,1 +1,10 @@
+public class ProxyUrlHelper {
+    public static String buildProxyUrl(String originalUrl) {
+        return "http://127.0.0.1:9978/?channel-id=" + 
+            Base64.encodeToString(originalUrl.getBytes(), Base64.URL_SAFE);
+    }
 
+    public static boolean isProxyUrl(String url) {
+        return url != null && url.startsWith("http://127.0.0.1:9978/?channel-id=");
+    }
+}
