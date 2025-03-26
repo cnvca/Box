89f6e5d52ffdf2ea6e9bc4702a0bbbcba37df86f
cnvca <122196660+cnvca@users.noreply.github.com>
2025-03-26 13:55:24 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -2,12 +2,12 @@ package com.github.tvbox.osc.util;
 
 import android.content.Context;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.github.tvbox.osc.api.ApiConfig;
 import com.github.tvbox.osc.bean.IJKCode;
 import com.github.tvbox.osc.player.EXOmPlayer;
 import com.github.tvbox.osc.player.IjkmPlayer;
 import com.github.tvbox.osc.player.render.SurfaceRenderViewFactory;
-import com.github.tvbox.osc.ui.activity.ItvDns;
 import com.orhanobut.hawk.Hawk;
 
 import org.json.JSONException;
@@ -23,23 +23,6 @@ import xyz.doikki.videoplayer.render.RenderViewFactory;
 import xyz.doikki.videoplayer.render.TextureRenderViewFactory;
 
 public class PlayerHelper {
-    private static final String TAG = "PlayerHelper";
-
-    public static String rewriteProxyUrl(String url) {
-        try {
-            if (url != null && url.startsWith("http://127.0.0.1:9978/?channel-id=")) {
-                return url.split("channel-id=")[1];
-            }
-            return url;
-        } catch (Exception e) {
-            return url;
-        }
-    }
-
-    public static void updateCfg(VideoView videoView) {
-        updateCfg(videoView, null, -1);
-    }
-
     public static void updateCfg(VideoView videoView, JSONObject playerCfg) {
         updateCfg(videoView, playerCfg, -1);
     }
@@ -49,99 +32,156 @@ public class PlayerHelper {
         int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
         String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "软解码");
         int scale = Hawk.get(HawkConfig.PLAY_SCALE, 0);
-
-        if (playerCfg != null) {
-            try {
-                playerType = playerCfg.getInt("pl");
-                ijkCode = playerCfg.getString("ijk");
-                scale = playerCfg.getInt("sc");
-            } catch (JSONException e) {
-                e.printStackTrace();
+        try {
+            playerType = playerCfg.getInt("pl");
+            //就我遇到的问题是 Exo 在 TextureView 黑屏 调整设置中的渲染模式无法生效
+            //renderType = playerCfg.getInt("pr");//该值无法修改，一旦确认该值后续无法进行修改 就是在设置选的 类型无法应用
+            ijkCode = playerCfg.getString("ijk");
+            scale = playerCfg.getInt("sc");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (forcePlayerType >= 0) playerType = forcePlayerType;
+        IJKCode codec = ApiConfig.get().getIJKCodec(ijkCode);
+        PlayerFactory playerFactory;
+        if (playerType == 1) {
+            playerFactory = new PlayerFactory<IjkmPlayer>() {
+                @Override
+                public IjkmPlayer createPlayer(Context context) {
+                    return new IjkmPlayer(context, codec);
+                }
+            };
+        } else if (playerType == 2) {
+            playerFactory = new PlayerFactory<EXOmPlayer>() {
+                @Override
+                public EXOmPlayer createPlayer(Context context) {
+                    return new EXOmPlayer(context);
+                }
+            };
+        } else if (playerType == 3) {
+            playerFactory = AliyunMediaPlayerFactory.create();
+        } else {
+            playerFactory = AndroidMediaPlayerFactory.create();
+        }
+        RenderViewFactory renderViewFactory = null;
+        if (playerType==2){
+            renderViewFactory = PlayerViewRenderViewFactory.create(renderType);
+        }else{
+            switch (renderType) {
+                case 0:
+                default:
+                    renderViewFactory = TextureRenderViewFactory.create();
+                    break;
+                case 1:
+                    renderViewFactory = SurfaceRenderViewFactory.create();
+                    break;
             }
         }
-
-        if (forcePlayerType >= 0) {
-            playerType = forcePlayerType;
-        }
-
-        PlayerFactory playerFactory = createPlayerFactory(playerType, ijkCode);
-        RenderViewFactory renderViewFactory = createRenderViewFactory(playerType, renderType);
 
         videoView.setPlayerFactory(playerFactory);
         videoView.setRenderViewFactory(renderViewFactory);
         videoView.setScreenScaleType(scale);
     }
 
-    private static PlayerFactory createPlayerFactory(int playerType, String ijkCode) {
-        switch (playerType) {
-            case 1:
-                return new PlayerFactory<IjkmPlayer>() {
-                    @Override
-                    public IjkmPlayer createPlayer(Context context) {
-                        return new IjkmPlayer(context, ApiConfig.get().getIJKCodec(ijkCode));
-                    }
-                };
-            case 2:
-                return new PlayerFactory<EXOmPlayer>() {
-                    @Override
-                    public EXOmPlayer createPlayer(Context context) {
-                        return new EXOmPlayer(context);
-                    }
-                };
-            case 3:
-                return AliyunMediaPlayerFactory.create();
-            default:
-                return AndroidMediaPlayerFactory.create();
-        }
-    }
+    public static void updateCfg(VideoView videoView) {
+        int playType = Hawk.get(HawkConfig.PLAY_TYPE, 0);
+        PlayerFactory playerFactory;
+        if (playType == 1) {
+            playerFactory = new PlayerFactory<IjkmPlayer>() {
+                @Override
+                public IjkmPlayer createPlayer(Context context) {
+                    return new IjkmPlayer(context, null);
+                }
+            };
 
-    private static RenderViewFactory createRenderViewFactory(int playerType, int renderType) {
-        if (playerType == 2) {
-            return PlayerViewRenderViewFactory.create(renderType);
+        } else if (playType == 2) {
+            playerFactory = new PlayerFactory<EXOmPlayer>() {
+                @Override
+                public EXOmPlayer createPlayer(Context context) {
+                    return new EXOmPlayer(context);
+                }
+            };
+        } else if (playType == 3) {
+            playerFactory = AliyunMediaPlayerFactory.create();
+        } else {
+            playerFactory = AndroidMediaPlayerFactory.create();
         }
-        return renderType == 1 ? SurfaceRenderViewFactory.create() : TextureRenderViewFactory.create();
+        int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
+        RenderViewFactory renderViewFactory = null;
+        switch (renderType) {
+            case 0:
+            default:
+                renderViewFactory = TextureRenderViewFactory.create();
+                break;
+            case 1:
+                renderViewFactory = SurfaceRenderViewFactory.create();
+                break;
+        }
+        videoView.setPlayerFactory(playerFactory);
+        videoView.setRenderViewFactory(renderViewFactory);
     }
 
     public static void init() {
         IjkMediaPlayer.loadLibrariesOnce(null);
-        System.setProperty("http.proxyHost", "127.0.0.1");
-        System.setProperty("http.proxyPort", String.valueOf(ItvDns.PORT));
-        System.setProperty("https.proxyHost", "127.0.0.1");
-        System.setProperty("https.proxyPort", String.valueOf(ItvDns.PORT));
     }
 
     public static String getPlayerName(int playType) {
-        switch (playType) {
-            case 1: return "IJK";
-            case 2: return "Exo";
-            case 3: return "阿里";
-            case 10: return "MX";
-            case 11: return "Reex";
-            case 12: return "Kodi";
-            default: return "系统";
+        if (playType == 1) {
+            return "IJK";
+        } else if (playType == 2) {
+            return "Exo";
+        } else if (playType == 3) {
+            return "阿里";
+        } else if (playType == 10) {
+            return "MX";
+        } else if (playType == 11) {
+            return "Reex";
+        } else if (playType == 12) {
+            return "Kodi";
+        } else {
+            return "系统";
         }
     }
 
     public static String getRenderName(int renderType) {
-        return renderType == 1 ? "SurfaceView" : "TextureView";
+        if (renderType == 1) {
+            return "SurfaceView";
+        } else {
+            return "TextureView";
+        }
     }
 
     public static String getScaleName(int screenScaleType) {
+        String scaleText = "默认";
         switch (screenScaleType) {
-            case VideoView.SCREEN_SCALE_DEFAULT: return "默认";
-            case VideoView.SCREEN_SCALE_16_9: return "16:9";
-            case VideoView.SCREEN_SCALE_4_3: return "4:3";
-            case VideoView.SCREEN_SCALE_MATCH_PARENT: return "填充";
-            case VideoView.SCREEN_SCALE_ORIGINAL: return "原始";
-            case VideoView.SCREEN_SCALE_CENTER_CROP: return "裁剪";
-            default: return "默认";
+            case VideoView.SCREEN_SCALE_DEFAULT:
+                scaleText = "默认";
+                break;
+            case VideoView.SCREEN_SCALE_16_9:
+                scaleText = "16:9";
+                break;
+            case VideoView.SCREEN_SCALE_4_3:
+                scaleText = "4:3";
+                break;
+            case VideoView.SCREEN_SCALE_MATCH_PARENT:
+                scaleText = "填充";
+                break;
+            case VideoView.SCREEN_SCALE_ORIGINAL:
+                scaleText = "原始";
+                break;
+            case VideoView.SCREEN_SCALE_CENTER_CROP:
+                scaleText = "裁剪";
+                break;
         }
+        return scaleText;
     }
 
     public static String getRootCauseMessage(Throwable th) {
-        for (int i = 0; i < 10 && th.getCause() != null; i++) {
-            th = th.getCause();
+        for (int i=0; i<10; i++) {
+            if (th.getCause() == null) return th.getLocalizedMessage();
+            else th = th.getCause();
         }
         return th.getLocalizedMessage();
     }
+
 }
