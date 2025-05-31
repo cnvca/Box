a848bcb1d1a1ebcc92cb179d622704350d19ef36
cnvca <122196660+cnvca@users.noreply.github.com>
2025-05-31 17:59:24 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -353,7 +353,8 @@ public class ApiConfig {
         // wallpaper
         wallpaper = DefaultConfig.safeJsonString(infoJson, "wallpaper", "");
         // 直播播放请求头
-        livePlayHeaders = infoJson.getAsJsonArray("livePlayHeaders");
+//        livePlayHeaders = infoJson.getAsJsonArray("livePlayHeaders");
+	livePlayHeaders = infoJson.getAsJsonArray("headers"); // 修改为 "headers"    
         // 远端站点源
         SourceBean firstSite = null;
         JsonArray sites = infoJson.has("video") ? infoJson.getAsJsonObject("video").getAsJsonArray("sites") : infoJson.get("sites").getAsJsonArray();
